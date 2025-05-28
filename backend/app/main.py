e73bafe0c43da9352efd3f705b902b7b7c904ffd
Thepthat Phaensanthia <phaensanthiat@gmail.com>
2025-05-28 17:11:05 +0700

    Edit main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,9 +34,9 @@ app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost", "127.0.0.1
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
